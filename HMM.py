762a220acacb01f72b77161eb6d7928f47f20ff1
jiangtaows <taoJiang_ict@163.com>
2016-03-31 16:40:05 +0800

    finish viterbi_method2 algorithm

--- HMM.py
+++ HMM.py
@@ -105,6 +105,8 @@ class HMM:
         (2) 从时间 t，状态 Si 出发，模型输出O＝O1O2 …OT， 根据后向变量定义，实现这一步的概率为bt(i)。
         于是： p（qt=si,O|u）=at(i)*bt(i)
         相当于 给出观测序列，找的不是整体最大可能的路线 而是 找到t时刻最可能的状态
+    返回t时刻最可能的状态
+    O(N*N*T)
     '''
     def viterbi_method1(self,observe_sequence,t):
         #通过前向传播计算t
@@ -140,3 +142,45 @@ class HMM:
                 max=a[i]*b[i]
                 s=i
         return s
+    '''
+    在给定模型 =(A, B, pi) 和观察序列 O＝O1O2 …OT 的情况下，选择在一定意义下“最优”的状态 序列 Q = q1 q2 … qT，使得该状态序列“最好地解 释”观察序列
+    第一种解释：
+        在给定模型 和观察序列O的条件 下求概率最大的状态序列 ，找的是整体最大可能的路线
+    返回最大可能的状态序列
+    O(N*N*T)
+    代码可以改进的地方：配合剪枝，减小搜索复杂度
+                        方案1：迭代过程中只保留 概率大于阈值的可能路径
+                        方案2：迭代过程中只保留 前k个概率最大的路径
+    '''
+    def viterbi_method2(self,observe_sequence):
+        path=[[0 for i in range(self.status_numbers)] for j in range(len(observe_sequence))]
+        a=[0.0 for i in range(self.status_numbers)]
+
+        index=self.Observations.index(observe_sequence[0])
+        for i in range(self.status_numbers):
+            a[i]=self.initial_state_probability_distribution[i]*self.probability_distribution_FromStatusObserve[i][index]
+        for t in range(1,len(observe_sequence)):
+            buff=a.copy()
+            index=self.Observations.index(observe_sequence[t])
+            for j in range(self.status_numbers):
+                max=0
+                max_index=0
+                for i in range(self.status_numbers):
+                    if buff[i]*self.trans_matrix_SiToSj[i][j]>max:
+                        max=buff[i]*self.trans_matrix_SiToSj[i][j]
+                        max_index=i
+                a[j]=buff[max_index]*self.trans_matrix_SiToSj[max_index][j]*self.probability_distribution_FromStatusObserve[j][index]
+                path[t][j]=max_index
+        max=0
+        max_index=0
+        for i in range(self.status_numbers):
+            if a[i]>max:
+                max=a[i]
+                max_index=i
+        status_sequence=[0 for i in range(len(observe_sequence))]
+        t=len(observe_sequence)-1
+        while t>=0:
+            status_sequence[t]=self.Status[max_index]
+            max_index=path[t][max_index]
+            t-=1
+        return status_sequence
--- demo.py
+++ demo.py
@@ -3,10 +3,11 @@ status=['a','b']
 observation=['m','n']
 trans_matrix=[[0.5,0.5],
               [0.5,0.5]]
-initial_status=[0.5,0.5]
+initial_status=[0,1]
 observation_probability_distribution=[[0.3,0.7],[0.3,0.7]]
 a=HMM(status,observation,trans_matrix,initial_status,observation_probability_distribution)
 print(a.forward_algorithm(['m','n']))
 print(a.backward_algorithm(['m','n']))
 
 print(a.viterbi_method1(['m','n'],1))
+print(a.viterbi_method2(['m','n']))
